139185d6671f32c11b7ee0fe0a88dd09d0f35819
Robson de farias <111597373+robsonfarias2963@users.noreply.github.com>
2024-01-27 19:13:10 -0300

    arrumei

---
+++ Exercicios/Desafio10/exemplo.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  var x = document.getElementById("contextText");
+  /* a variável x é o texto dentro da div com id="contextText" /
+  if (x.style.display === "none") {
+    / se o sytle da div for none, ou seja, estiver invisível... /
+    x.style.display = "block";
+    / ... então faça o style ser block, ou seja, ficar visível... /
+  } else {
+    x.style.display = "none";
+    / ... caso contrário, (se já estiver visível) faça sumir.*/
+  }
